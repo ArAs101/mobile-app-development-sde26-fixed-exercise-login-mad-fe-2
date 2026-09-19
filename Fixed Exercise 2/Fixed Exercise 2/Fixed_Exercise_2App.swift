7f157e490eb9f4cebf48694ebc8c7d394aa59219
ArAs101 <armin.assadzadeh@stud.hcw.ac.at>
2026-09-19 16:47:29 +0200

    Submission

---
+++ Fixed Exercise 2/Fixed Exercise 2/Fixed_Exercise_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Fixed_Exercise_2App.swift
+//  Fixed Exercise 2
+//
+//  Created by Armin Assadzadeh on 19.09.26.
+//
+
+import SwiftUI
+
+@main
+struct Fixed_Exercise_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
